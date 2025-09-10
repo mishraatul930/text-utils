03daa66f3daf6286e2ae3927d1fecb382a4870ce
Atul Mishra <40742234+mishraatul930@users.noreply.github.com>
2025-09-10 10:55:50 +0530

    Extra Functionality added

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -18,23 +18,34 @@ export default function TextForm(props) {
   };
   return (
     <>
-      <div className="mb-3">
+      <div className="container">
         <h1 className="my-3">{props.heading}</h1>
-        <label htmlFor="myBox" className="form-label"></label>
-        <textarea
-          className="form-control"
-          value={text}
-          onChange={handleOnChange}
-          id="myBox"
-          rows="8"
-        ></textarea>
+        <div className="mb-3">
+          <label htmlFor="myBox" className="form-label"></label>
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary" onClick={handleUpClick}>
+          Convert to UPPERCASE
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleLowClick}>
+          Convert to lowercase
+        </button>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>
-        Convert to UPPERCASE
-      </button>
-      <button className="btn btn-primary mx-3" onClick={handleLowClick}>
-        Convert to lowercase
-      </button>
+      <div className="container my-3">
+        <h1>Your Text Summary</h1>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes to read.</p>
+        <h3>Preview Text</h3>
+        <p>{text}</p>
+      </div>
     </>
   );
 }
